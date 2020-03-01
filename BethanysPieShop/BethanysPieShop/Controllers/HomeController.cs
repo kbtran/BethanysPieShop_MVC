63e20b891fc4858687e0d29dfc42ae706fa45c96
Bruce Tran <bruce.tran@gmail.com>
2020-03-01 00:40:18 -0500

    Update HomeController and Index.cshtml. Add HomeViewModel and _Carousel.cshtml.

--- BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+        
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
         }
     }
 }
