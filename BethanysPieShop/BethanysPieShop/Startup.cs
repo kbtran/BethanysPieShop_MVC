8e4343ea7414f1b1cb9a16fc63acf758e3757589
Bruce Tran <bruce.tran@gmail.com>
2020-02-29 22:49:53 -0500

    Add DbContext, remove mocks and use SQL Server

--- BethanysPieShop/BethanysPieShop/Startup.cs
+++ BethanysPieShop/BethanysPieShop/Startup.cs
@@ -1,18 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BethanysPieShop
 {
+    
+
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,8 +27,16 @@ namespace BethanysPieShop
             services.AddControllersWithViews();
 
             // Add mocks
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-            services.AddScoped<IPieRepository, MockPieRepository>();
+            // services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            // services.AddScoped<IPieRepository, MockPieRepository>();
+
+            // Add SQL Server
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Use SQL Server
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
